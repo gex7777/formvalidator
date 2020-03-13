8beb7f29b4c85a08346ea9d5cef78d8aefd04d39
gex7777 <gex7777@gmail.com>
2020-03-13 18:10:10 +0530

    add refractoring

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+
 //show input error message
 function showError(input, message) {
 	const formControl = input.parentElement;
@@ -10,40 +11,35 @@ function showError(input, message) {
 	const small = formControl.querySelector("small");
 	small.innerText = message;
 }
+
+//show sucess outline
 function showSuccess(input) {
 	const formControl = input.parentElement;
 	formControl.className = "form-control sucess";
 }
+//check required fields
+function checkRequired(inputArr) {
+	inputArr.forEach(function(input) {
+		if (input.value === "") {
+			showError(input, `${getFieldName(input.id)} is invalid`);
+		} else {
+			showSuccess(input);
+		}
+	});
+}
+
+//return capatilized id
+function getFieldName(input) {
+	return input.charAt(0).toUpperCase() + input.slice(1);
+}
+//check valid email
 function isValidEmail(email) {
 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
 }
+
 //event listeners
 form.addEventListener("submit", function(e) {
 	e.preventDefault();
-	if (username.value === "") {
-		showError(username, "username required");
-	} else {
-		showSuccess(username);
-	}
-
-	if (email.value === "") {
-		showError(email, "email required");
-	} else if (!isValidEmail(email.value)) {
-		showError(email, "email is invalid");
-	} else {
-		showSuccess(email);
-	}
-
-	if (password.value === "") {
-		showError(password, "password required");
-	} else {
-		showSuccess(password);
-	}
-
-	if (password2.value === "") {
-		showError(password2, "password 2 required");
-	} else {
-		showSuccess(password2);
-	}
+	checkRequired([username, email, password, password2]);
 });
